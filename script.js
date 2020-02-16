6dbd566048fea9907a5d3532400d71c4772a1d26
Patrick <patr983e@stud.kea.dk>
2020-02-16 23:02:31 +0100

    clean json data

--- script.js
+++ script.js
@@ -1,27 +1,80 @@
 window.addEventListener("DOMContentLoaded", init);
 
+const studentsArray = [];
+
+let fullNameArray = {};
+
+const studentPrototype = {
+  firstName: "",
+  lastName: "",
+  middleName: "",
+  nickName: "",
+  imageFileName: "",
+  house: ""
+};
+
 function init() {
   getStudents();
 }
 
 function getStudents() {
-  fetch("students1991.json")
+  fetch("https://petlatkea.dk/2020/hogwarts/students.json")
     .then(response => response.json())
     .then(analyseStudents);
 }
 
 function analyseStudents(students) {
-  students.forEach(showStudents);
+  students.forEach(cleanStudent);
+}
+
+function cleanStudent(student) {
+  //create new object
+  let newStudent = Object.create(studentPrototype);
+
+  //first name and last name
+  let name = student.fullname;
+
+  //verify and delete wrong characters and set to lowercase
+  fullNameArray = name.split(" ");
+
+  fullNameArray.forEach(changeName);
+  console.log(fullNameArray);
+
+  //assign name values to the object
+  newStudent.firstName = fullNameArray[0];
+  newStudent.lastName = fullNameArray[fullNameArray.length - 1];
+  if (fullNameArray.length > 2) {
+    newStudent.middleName = fullNameArray.slice(1, fullNameArray.length - 1);
+    newStudent.nickName = fullNameArray.slice(1, fullNameArray.length - 1);
+  }
+  newStudent.imageFileName = "student_image.png";
+  newStudent.house = student.house.toLowerCase();
+
+  console.log(newStudent);
+
+  //add new object to array
+  studentsArray.push(newStudent);
+  console.log(studentsArray);
+
+  showStudents(newStudent);
+}
+
+function getArrayOfStudents() {
+  studentsArray.forEach(showStudents);
+  console.log("ARRAY 0 IS: " + studentsArray[0]);
 }
 
 function showStudents(student) {
+  let studentFullName;
+
   const template = document.querySelector(".studentTemplate").content;
   const templateCopy = template.cloneNode(true);
 
   const studentTitle = templateCopy.querySelector(".studentName");
   const studentInfo = templateCopy.querySelector(".studentHouse");
 
-  studentTitle.textContent = student.fullname;
+  studentFullName = `${student.firstName} ${student.lastName}`;
+  studentTitle.textContent = studentFullName;
   studentInfo.textContent = student.house;
 
   //add a data-house value for each tr
@@ -40,7 +93,7 @@ function showStudents(student) {
     contentModal.setAttribute("data-design", student.house.toLowerCase());
 
     //Show content
-    modal.querySelector(".modal_header .name").textContent = student.fullname;
+    modal.querySelector(".modal_header .name").textContent = studentFullName;
     //Show the image with the name of the house in the path
     modal.querySelector(".modal_header .house_image").src = `houses_imgs/${student.house.toLowerCase()}.png`;
     //////////////////////////////
@@ -54,4 +107,34 @@ function showStudents(student) {
   });
 
   document.querySelector(".listOfStudents_table tbody").appendChild(templateCopy);
+}
+
+function changeName(name, index) {
+  //if the first name is empty
+  if (fullNameArray[0] == "") {
+    fullNameArray.shift(); //delete the first element of the array
+  }
+
+  //if the last name is empty
+  if (fullNameArray[fullNameArray.length - 1] == "") {
+    fullNameArray.pop(); //delete the first element of the array
+  }
+
+  //convert name to array
+  let singleNameArray = Array.from(fullNameArray[index]);
+
+  //delete or change character
+  singleNameArray.forEach((character, index) => {
+    if (character == '"' || character == `"\"`) {
+      singleNameArray[index] = "";
+    } else if (index == 0) {
+      singleNameArray[0] = singleNameArray[0].toUpperCase();
+    } else {
+      singleNameArray[index] = singleNameArray[index].toLowerCase();
+    }
+  });
+
+  //update that string in the fullNameArray after changing each character
+  let singleNameString = singleNameArray.join("");
+  fullNameArray[index] = singleNameString;
 }
